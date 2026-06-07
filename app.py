93cdf04e995d64e178dab3d0c2dec599c5696584
NE-Ozcan <emirebenike@gmail.com>
2026-06-07 16:56:38 +0200

    Validatietest: Tekstaanpassing voor demo

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "<h1>Welkom bij het Inspire Offices Beheerportaal!</h1>" + \
-             "<p>Wij zijn een deel van de toekomst.</p>"
+             "<p>Houd je devices in de gaten.</p>"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
