8a87cbe2443b21485a8feea41b27dcb841bddda4
NE-Ozcan <emirebenike@gmail.com>
2026-06-08 15:31:15 +0200

    Demo Test 3:Endd

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "<h1>Welkom bij het Inspire Offices Beheerportaal!</h1>" + \
-             "<p>Gemaakt door:Yassin, Mustafa, Emir, Musa, Moh en Krijn.</p>"
+             "<p>Gemaakt door:Yassin , Mustafa, Emir, Musa, Moh en Krijn.</p>"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
